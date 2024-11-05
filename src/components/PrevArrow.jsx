01022c3fb498660bacf4a8b0c06eba8c21a8cd4d
MdKhairulHassan <darknessmoon76@gmail.com>
2024-11-06 02:18:32 +0600

    update

--- src/components/PrevArrow.jsx
+++ src/components/PrevArrow.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { FaLongArrowAltLeft } from "react-icons/fa";
 
 const PrevArrow = (props) => {
-const { className, style, onClick } = props;
+const { className, onClick } = props;
   return (
     <div
       className={`bg-BorderColor rounded-full inline-block p-3 text-xl text-white absolute z-10 top-[40%] left-2 ${className}`}
-      // style={{ ...style, display: "block", background: "red" }}
       onClick={onClick}
     ><FaLongArrowAltLeft /></div>
   )
